b22a8d7e14010e84bd962f9b4a32292fc4d45b6e
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-27 15:44:22 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Space_Grotesk } from "next/font/google"
 import "./globals.css"
+import Header from "@/components/Header"
 
 const font = Space_Grotesk({ subsets: ["latin"] })
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-very-dark-grey text-white text-base">
       <body className={font.className}>
+        <Header />
         <main>
           {children}
         </main>
