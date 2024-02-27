666fac9ebbfac3cfe97a7687bcd11f38dd311a74
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-27 18:55:51 +0200

    Update Projects.jsx

--- app/Projects.jsx
+++ app/Projects.jsx
@@ -1,6 +1,7 @@
 import ContactMe from "@/components/ContactMe"
 import UList from "@/components/UList"
 import Image from "next/image"
+import Link from "next/link"
 
 const projects = [
     {
@@ -29,15 +30,25 @@ export default () => (
             className=""
             items={projects}
             itemHandler={({ name, thumbnails: { small, large }, links: { code, site }, tech }) => (
-                <div>
-                    <div className="relative h-[250px]">
-                        <Image
-                            alt={name}
-                            src={small}
-                            fill
+                <div className="grid gap-5 uppercase">
+                    <Image
+                        alt={name}
+                        src={small}
+                        width={800}
+                        height={800}
+                    />
+                    <div>
+                        <h2 className="text-md font-bold">{name}</h2>
+                        <UList
+                            className="flex gap-5 text-light-grey"
+                            items={tech}
+                            itemHandler={item => <p>{item}</p>}
                         />
                     </div>
-                    <h2 className="text-md font-bold">{name}</h2>
+                    <div className="flex gap-8">
+                        <Link className="btn" href={site} target="_blank">view project</Link>
+                        <Link className="btn" href={code} target="_blank">view code</Link>
+                    </div>
                 </div>
             )}
         />
