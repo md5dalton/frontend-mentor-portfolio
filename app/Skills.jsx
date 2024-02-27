232d91a2d61007a3c3e425e8c70ba1f983a33ef8
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-27 18:41:43 +0200

    Create Skills.jsx

---
+++ app/Skills.jsx
@@ -0,0 +1,25 @@
+import UList from "@/components/UList"
+
+const skills = [
+    {name: "HTML", years: 4},
+    {name: "CSS", years: 4},
+    {name: "Javascript", years: 4},
+    {name: "Accessibility", years: 4},
+    {name: "React", years: 3},
+    {name: "Sass", years: 3},
+]
+
+export default () => (
+    <section className="page-w text-center">
+        <UList
+            className="border-y-2 border-y-light-grey py-10 grid gap-6"
+            items={skills}
+            itemHandler={({ name, years }) => (
+                <div>
+                    <h2 className="text-md font-bold">{name}</h2>
+                    <p className="text-light-grey">{`${years} Years Experience`}</p>
+                </div>
+            )}
+        />
+    </section>
+)
