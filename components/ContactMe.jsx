c886b9365c96d4aa41422b5f1e5f0d2d61311c18
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-27 18:55:54 +0200

    Update ContactMe.jsx

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -1,5 +1,5 @@
 export default () => (
     <button
-        className="border-b-2 border-b-light-green tracking-[2.2px] font-boldh-10"
+        className="border-b-2 border-b-light-green tracking-[2.2px] font-bold h-10"
     >CONTACT ME</button>
 )
