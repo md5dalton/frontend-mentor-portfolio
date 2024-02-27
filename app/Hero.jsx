e2b3b3992158518c92f85048436bdefa1c6dddfb
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-27 18:41:20 +0200

    Create Hero.jsx

---
+++ app/Hero.jsx
@@ -0,0 +1,11 @@
+import ContactMe from "@/components/ContactMe"
+
+export default () => (
+    <section className="page-w text-center">
+        <h1 className="text-[40px] leading-[40px] font-bold">
+            Nice to meet you! I'm <span className="border-b-2 border-b-light-green">Majara Mpiti</span>
+        </h1>
+        <p className="my-6">Based in Lesotho, I'm a front-end developer passionate about building accessible web apps that users love</p>
+        <ContactMe />
+    </section>
+)
