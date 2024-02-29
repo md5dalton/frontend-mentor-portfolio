23da5d4e2784b1723756ceb432ea7e241ed89b3f
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-02-29 17:36:48 +0200

    Update Hero.jsx

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -5,7 +5,7 @@ export default () => (
         <h1 className="text-[40px] leading-[40px] font-bold">
             Nice to meet you! I'm <span className="border-b-2 border-b-light-green">Majara Mpiti</span>
         </h1>
-        <p className="my-6">Based in Lesotho, I'm a front-end developer passionate about building accessible web apps that users love</p>
+        <p className="my-6 opacity-50">Based in Lesotho, I'm a front-end developer passionate about building accessible web apps that users love</p>
         <ContactMe />
     </section>
 )
