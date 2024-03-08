963ccf2e7cee986576ccebaebc7fc012970702c5
md5dalton <md5daltonggs@gmail.com>
2024-03-08 18:22:18 +0200

    Update ContactMe.jsx

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -1,5 +1,5 @@
+import Button from "./Button"
+
 export default () => (
-    <button
-        className="border-b-2 border-b-light-green tracking-[2.2px] font-bold h-10"
-    >CONTACT ME</button>
+    <Button>CONTACT ME</Button>
 )
