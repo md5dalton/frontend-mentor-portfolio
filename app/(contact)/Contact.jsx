fe14ebfdf7fffe58fe04956f80ca2261a091b9b3
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-03-10 11:01:01 +0200

    Create Contact.jsx

---
+++ app/(contact)/Contact.jsx
@@ -0,0 +1,11 @@
+import Form from "./Form"
+
+export default () => (
+    <section className="page-w text-center grid gap-12">
+        <div>
+            <h2 className="text-[40px] leading-[40px] font-bold">Contact</h2>
+            <p className="mt-5 opacity-50">I would love to hear about your project and how I could help. Please fill in the form, and I'll get back to you as soon as possible.</p>
+        </div>
+        <Form />
+    </section>
+)
