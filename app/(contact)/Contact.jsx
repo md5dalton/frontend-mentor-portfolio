00bab05bc568abc7f1ed2050f1f013cd3b7bb36a
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-13 10:11:42 +0200

    Update Contact.jsx

--- app/(contact)/Contact.jsx
+++ app/(contact)/Contact.jsx
@@ -2,7 +2,7 @@ import Form from "./Form"
 import Messanger from "./Messanger"
 
 export default () => (
-    <section className="page-w text-center grid gap-12" id="contact">
+    <section className="text-center grid gap-12" id="contact">
         <div>
             <h2 className="text-[40px] leading-[40px] font-bold">Contact</h2>
             <p className="mt-5 opacity-50">I would love to hear about your project and how I could help. Please fill in the form, and I'll get back to you as soon as possible.</p>
