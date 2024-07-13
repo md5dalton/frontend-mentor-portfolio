d3a52dea135e4e5918e8383e27dd6f84d48ac57a
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-13 11:53:25 +0200

    Update Footer.jsx

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -4,8 +4,8 @@ import Nav from "./Nav"
 export default () => (
     <footer className="bg-dark-grey py-16 grid gap-20">
         <div className="page space-y-20">
-            {/* <Contact /> */}
-            <div className="border-t-[2px] border-t-light-grey pt-5">
+            <Contact />
+            <div className="border-t-[2px] border-t-light-grey py-10 md:py-8 lg:py-12">
                 <Nav />
             </div>
         </div>
