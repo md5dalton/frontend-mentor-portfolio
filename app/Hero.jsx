981a825ee89156321be009bc3355b8aa04c5c638
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-17 11:53:06 +0200

    Update Hero.jsx

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -2,12 +2,12 @@ import ContactMe from "@/components/ContactMe"
 import Section from "@/components/Section"
 
 export default () => (
-    <Section>
-        <div className="max-w-[440px]">
+    <Section className="mt-64 md:mt-14">
+        <div className="max-w-[440px] lg:max-w-[706px]">
             <h1 className="text-h-xl">
-                Nice to meet you! I'm <span className="border-b-2 border-b-light-green">Majara Mpiti</span>
+                Nice to <br className="hidden md:inline lg:hidden" /> meet you! I'm <span className="border-b-2 border-b-light-green">Mj Mpiti</span>
             </h1>
-            <p className="my-6 opacity-50">Based in Lesotho, I'm a front-end developer passionate about building accessible web apps that users love</p>
+            <p className="my-6 opacity-50 max-w-[440px]">Based in Lesotho, I'm a front-end developer passionate about building accessible web apps that users love</p>
             <ContactMe />
         </div>
     </Section>
