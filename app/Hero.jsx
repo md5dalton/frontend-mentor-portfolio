57212fedb115f8ceb992748dd4d4f6e9d2a824df
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-17 15:41:19 +0200

    Update Hero.jsx

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -5,7 +5,7 @@ export default () => (
     <Section className="mt-64 md:mt-14">
         <div className="md:max-w-[440px] lg:max-w-[706px]">
             <h1 className="text-h-xl">
-                Nice to <br className="hidden md:inline lg:hidden" /> meet you! I'm <span className="border-b-2 border-b-light-green">Mj Mpiti</span>
+                Nice to <br className="hidden md:inline lg:hidden" /> meet you! I'm <span className="border-b-2 border-b-light-green">Mjr Mpiti</span>
             </h1>
             <p className="my-6 opacity-50 md:max-w-[440px]">Based in Lesotho, I'm a front-end developer passionate about building accessible web apps that users love</p>
             <ContactMe />
