fc77447e31991a8011447660db1eaa82def1a34f
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-17 16:22:44 +0200

    Update ProfilePicture.jsx

--- components/ProfilePicture.jsx
+++ components/ProfilePicture.jsx
@@ -7,23 +7,19 @@ export default () => (
             flex justify-center md:justify-end
         "
     >
-        <div className="lg:w-full lg:mx-auto max-w-[1100px] lg:px-8 lg:grid lg:justify-end">
+        <div className="max-w-[1100px] lg:w-full lg:mx-auto lg:px-8 lg:grid lg:justify-end">
             <picture>
                 <source
                     media="(min-width: 768px)"
-                    srcSet="/images/image-profile-tablet.webp"
+                    srcSet="/images/profile-tablet.png"
                 />
                 <source
                     media="(min-width: 1024px)"
-                    srcSet="/images/image-profile-desktop.webp"
+                    srcSet="/images/profile-desktop.png"
                 />
                 <Image
-                    className="md:h-[600px] md:w-[322px]
-                        
-
-                    "
-                    // src="/images/image-profile-mobile.webp"
-                    src="/images/Untitled-2.png"
+                    className="md:h-[600px] md:w-[322px]"
+                    src="/images/profile-mobile.png"
                     height={380}
                     width={170}
                     alt=""
