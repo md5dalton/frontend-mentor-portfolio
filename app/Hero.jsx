3cc5e88a0fd74856b18e493c9a31ba8639a00766
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-17 10:47:39 +0200

    Update Hero.jsx

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -3,8 +3,8 @@ import Section from "@/components/Section"
 
 export default () => (
     <Section>
-        <div className="max-w-[445px] text-center">
-            <h1 className="text-[40px] leading-[40px] font-bold">
+        <div className="max-w-[440px]">
+            <h1 className="text-h-xl">
                 Nice to meet you! I'm <span className="border-b-2 border-b-light-green">Majara Mpiti</span>
             </h1>
             <p className="my-6 opacity-50">Based in Lesotho, I'm a front-end developer passionate about building accessible web apps that users love</p>
