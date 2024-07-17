2c97d3bc92f5c8ababb771af58bcbb097d97e909
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-17 10:47:47 +0200

    Update Contact.jsx

--- app/(contact)/Contact.jsx
+++ app/(contact)/Contact.jsx
@@ -2,9 +2,16 @@ import Form from "./Form"
 import Messanger from "./Messanger"
 
 export default () => (
-    <section className="text-center grid gap-12" id="contact">
+    <section
+        className="
+            text-center grid gap-12
+            max-w-[445px] mx-auto
+            lg:grid-cols-2 lg:max-w-none lg:text-left
+        "
+        id="contact"
+    >
         <div>
-            <h2 className="text-[40px] leading-[40px] font-bold">Contact</h2>
+            <h2 className="text-h-xl">Contact</h2>
             <p className="mt-5 opacity-50">I would love to hear about your project and how I could help. Please fill in the form, and I'll get back to you as soon as possible.</p>
         </div>
         <Form />
