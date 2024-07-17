c3be04e24dd4ac0f30b6f421263ab7d7768470f6
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-07-17 13:32:18 +0200

    Update ProfilePicture.jsx

--- components/ProfilePicture.jsx
+++ components/ProfilePicture.jsx
@@ -3,30 +3,31 @@ import Image from "next/image"
 export default () => (
     <section
         className="
-            absolute -z-10 top-0 left-1/2 -translate-x-1/2
-            md:translate-x-0 md:right-0 md:left-auto
-            lg:max-w-[1100px]
+            absolute -z-10 top-0 w-full
+            flex justify-center md:justify-end
         "
     >
-        <picture>
-            <source
-                media="(min-width: 768px)"
-                srcSet="/images/image-profile-tablet.webp"
-            />
-            <source
-                media="(min-width: 1024px)"
-                srcSet="/images/image-profile-desktop.webp"
-            />
-            <Image
-                className="md:h-[600px] md:w-[322px]
-                    
+        <div className="lg:w-full lg:mx-auto max-w-[1100px] lg:px-8 lg:grid lg:justify-end">
+            <picture>
+                <source
+                    media="(min-width: 768px)"
+                    srcSet="/images/image-profile-tablet.webp"
+                />
+                <source
+                    media="(min-width: 1024px)"
+                    srcSet="/images/image-profile-desktop.webp"
+                />
+                <Image
+                    className="md:h-[600px] md:w-[322px]
+                        
 
-                "
-                src="/images/image-profile-mobile.webp"
-                height={380}
-                width={170}
-                alt=""
-            />
-        </picture>
+                    "
+                    src="/images/image-profile-mobile.webp"
+                    height={380}
+                    width={170}
+                    alt=""
+                />
+            </picture>
+        </div>
     </section>
 )
