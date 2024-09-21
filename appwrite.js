75a976af693ec440fd7e6bf176a2c876aa9c20ef
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-09-21 19:26:59 +0200

    Update appwrite.js

--- appwrite.js
+++ appwrite.js
@@ -1,8 +1,17 @@
-import { Client, Storage } from "appwrite"
+import { Client, Storage, Databases } from "appwrite"
 
 const client = new Client()
     .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject(process.env.PROJECT_ID)
+    .setProject(process.env.APPWRITE_PROJECT_ID)
 
 const storage = new Storage(client)
+const databases = new Databases(client)
 
+
+export const getSkills = async () => databases.listDocuments(
+    process.env.APPWRITE_DATABASE_ID,
+    "66eef22c0032d6d23141",
+        // [
+        //     Query.equal('title', 'Hamlet')
+        // ]
+)
